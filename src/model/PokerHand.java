98b7be7f4ff1d7fc35b2a13bf43869d97223f551
Dante Mienie <dantemienie@dhcp-10-132-200-184.uawifi.arizona.edu>
2024-04-22 22:40:36 -0700

    Finished and passing test cases

--- src/model/PokerHand.java
+++ src/model/PokerHand.java
@@ -9,191 +9,187 @@ import java.util.Collections;
  */
 
 public class PokerHand implements Comparable<PokerHand> {
-	private ArrayList<Integer> playerHand = new ArrayList<>();
-	private int highestCard;
-	private int highestCard2Pairs;
-	private int kicker2Pairs;
 
-	private String handName;
-	private int handRank = 10;
+	private ArrayList<Card> playerHand = new ArrayList<>();
+
+	private int highestCard;
+	public int handRank = 10;
 
 	public PokerHand(Card c1, Card c2, Card c3, Card c4, Card c5) {
-		playerHand.add(c1.getValue());
-		playerHand.add(c2.getValue());
-		playerHand.add(c3.getValue());
-		playerHand.add(c4.getValue());
-		playerHand.add(c5.getValue());
+		playerHand.add(c1);
+		playerHand.add(c2);
+		playerHand.add(c3);
+		playerHand.add(c4);
+		playerHand.add(c5);
 
 		Collections.sort(playerHand);
-		highestCard = Collections.max(playerHand);
-		highestCard2Pairs = Collections.max(playerHand);
-		kicker2Pairs = Collections.max(playerHand);
+		highestCard = 0;
 
-		// Royal flush
-		if (isFlush(c1, c2, c3, c4, c5) && playerHand.get(4) == 14) {
-			handName = "Royal flush";
-			handRank = 1;
-			return;
-		}
-
-		// Straight flush
-		if ((isFlush(c1, c2, c3, c4, c5)
-				&& (playerHand.get(1) == playerHand.get(0) + 1 && playerHand.get(2) == playerHand.get(1) + 1
-						&& playerHand.get(3) == playerHand.get(2) + 1 && playerHand.get(4) == playerHand.get(3) + 1))) {
-			handName = "Straight flush";
-			handRank = 2;
-			return;
-		}
-		// Four of a kind
-		if (c1.getRank() == c2.getRank() && c2.getRank() == c3.getRank() && c3.getRank() == c4.getRank()
-				|| c2.getRank() == c3.getRank() && c3.getRank() == c4.getRank() && c4.getRank() == c5.getRank()) {
-			handName = "Four of a kind";
-			handRank = 3;
-			return;
-		}
-		// Full house
-		if ((c1.getRank() == c2.getRank() && c2.getRank() == c3.getRank()
-				|| (c3.getRank() == c4.getRank() && c4.getRank() == c5.getRank())
-						&& (c1.getRank() == c2.getRank() || c4.getRank() == c5.getRank()))) {
-			handName = "Full house";
-			highestCard = c3.getValue();
-			handRank = 4;
-			return;
-		}
-
-		// Flush
-		if (isFlush(c1, c2, c3, c4, c5)) {
-			handName = "Flush";
-			handRank = 5;
-		}
-		// Straight
-		if (playerHand.get(1) == playerHand.get(0) + 1 && playerHand.get(2) == playerHand.get(1) + 1
-				&& playerHand.get(3) == playerHand.get(2) + 1 && playerHand.get(4) == playerHand.get(3) + 1) {
-			handName = "Straight";
-			handRank = 6;
-			return;
-		}
-
-		// Three of a kind
-		if ((c1.getRank() == c2.getRank() && c2.getRank() == c3.getRank())
-				|| (c2.getRank() == c3.getRank() && c3.getRank() == c4.getRank()
-						|| (c3.getRank() == c4.getRank() && c4.getRank() == c5.getRank()))) {
-			handName = "Three of a kind";
-			handRank = 7;
-			if (c1.getRank() == c2.getRank() && c2.getRank() == c3.getRank()) {
-				highestCard = Integer.max(c4.getValue(), c5.getValue());
-			} else if (c2.getRank() == c3.getRank() && c3.getRank() == c4.getRank()) {
-				highestCard = Integer.max(c1.getValue(), c5.getValue());
-			} else {
-				highestCard = Integer.max(c1.getValue(), c2.getValue());
-			}
-			return;
-		}
-		// Two pair
-		if ((c1.getRank() == c2.getRank() && c3.getRank() == c4.getRank())
-				|| (c1.getRank() == c2.getRank() && c4.getRank() == c5.getRank())
-				|| (c2.getRank() == c3.getRank() && c4.getRank() == c5.getRank())) {
-			handName = "Two pair";
-			handRank = 8;
-
-			int tempMax = 0;
-			int tempMin = 0;
-
-			if (c1.getRank() == c2.getRank() && c3.getRank() == c4.getRank()) {
-				tempMax = Integer.max(c1.getValue(), c3.getValue());
-				tempMin = Integer.min(c1.getValue(), c3.getValue());
-				kicker2Pairs = c5.getValue();
-				return;
-			}
-
-			if (c1.getRank() == c2.getRank() && c4.getRank() == c5.getRank()) {
-				tempMax = Integer.max(c2.getValue(), c4.getValue());
-				tempMin = Integer.min(c2.getValue(), c4.getValue());
-				kicker2Pairs = c3.getValue();
-				return;
-
-			}
-			if (c2.getRank() == c3.getRank() && c4.getRank() == c5.getRank()) {
-				tempMax = Integer.max(c2.getValue(), c4.getValue());
-				tempMin = Integer.max(c2.getValue(), c4.getValue());
-				kicker2Pairs = c1.getValue();
-				return;
-
-			}
-
-			highestCard = tempMax;
-			highestCard2Pairs = tempMin;
-			return;
-
-		}
-		// Pair
-		if ((c1.getRank() == c2.getRank()) || (c2.getRank() == c3.getRank())
-				|| (c3.getRank() == c4.getRank() || c4.getRank() == c5.getRank())) {
-			handName = "Pair";
-			handRank = 9;
-			int pairValue = 0;
-			if (c1.getRank() == c2.getRank()) {
-				pairValue = c1.getValue();
-			} else if (c2.getRank() == c3.getRank()) {
-				pairValue = c2.getValue();
-			} else if (c3.getRank() == c4.getRank()) {
-				pairValue = c3.getValue();
-			} else if (c4.getRank() == c5.getRank()) {
-				pairValue = c4.getValue();
-			}
-			highestCard = pairValue;
-
-		}
 	}
 
-	private boolean isFlush(Card c1, Card c2, Card c3, Card c4, Card c5) {
-		if (c1.getSuit() == c2.getSuit() && c2.getSuit() == c3.getSuit() && c3.getSuit() == c4.getSuit()
-				&& c4.getSuit() == c5.getSuit()) {
+	// isFlush
+	private boolean isFlush(ArrayList<Card> playerHand) {
+		if (playerHand.get(0).getSuit() == playerHand.get(1).getSuit()
+				&& playerHand.get(1).getSuit() == playerHand.get(2).getSuit()
+				&& playerHand.get(2).getSuit() == playerHand.get(3).getSuit()
+				&& playerHand.get(3).getSuit() == playerHand.get(4).getSuit()) {
 			return true;
 		}
 		return false;
 	}
 
+	// Royal flush
+	private boolean isRoyalFlush(ArrayList<Card> playerHand) {
+		if (isFlush(playerHand) && playerHand.get(0).getValue() == 10 && playerHand.get(1).getValue() == 11
+				&& playerHand.get(2).getValue() == 12 && playerHand.get(3).getValue() == 13
+				&& playerHand.get(4).getValue() == 14) {
+			return true;
+		}
+		return false;
+	}
+
+	// Straight flush
+	private boolean isStraightFlush(ArrayList<Card> playerHand) {
+		if ((isFlush(playerHand) && (playerHand.get(1).getValue() == playerHand.get(0).getValue() + 1
+				&& playerHand.get(2).getValue() == playerHand.get(1).getValue() + 1
+				&& playerHand.get(3).getValue() == playerHand.get(2).getValue() + 1
+				&& playerHand.get(4).getValue() == playerHand.get(3).getValue() + 1))) {
+			return true;
+		}
+		return false;
+	}
+
+	// Four of a kind
+	private boolean isFourOfKind(ArrayList<Card> playerHand) {
+		if (playerHand.get(0).getRank() == playerHand.get(1).getRank()
+				&& playerHand.get(1).getRank() == playerHand.get(2).getRank()
+				&& playerHand.get(2).getRank() == playerHand.get(3).getRank()
+				|| playerHand.get(1).getRank() == playerHand.get(2).getRank()
+						&& playerHand.get(2).getRank() == playerHand.get(3).getRank()
+						&& playerHand.get(3).getRank() == playerHand.get(4).getRank()) {
+			return true;
+		}
+		return false;
+	}
+
+	// Full house
+	private boolean isFullHouse(ArrayList<Card> playerHand) {
+		if ((playerHand.get(0).getRank() == playerHand.get(1).getRank()
+				&& playerHand.get(1).getRank() == playerHand.get(2).getRank()
+				&& playerHand.get(3).getRank() == playerHand.get(4).getRank())
+				|| (playerHand.get(0).getRank() == playerHand.get(1).getRank()
+						&& playerHand.get(2).getRank() == playerHand.get(3).getRank()
+						&& playerHand.get(3).getRank() == playerHand.get(4).getRank())) {
+			highestCard = playerHand.get(2).getValue();
+			return true;
+		}
+		return false;
+	}
+
+	// Straight
+	private boolean isStraight(ArrayList<Card> playerHand) {
+		if (playerHand.get(1).getValue() == playerHand.get(0).getValue() + 1
+				&& playerHand.get(2).getValue() == playerHand.get(1).getValue() + 1
+				&& playerHand.get(3).getValue() == playerHand.get(2).getValue() + 1
+				&& playerHand.get(4).getValue() == playerHand.get(3).getValue() + 1) {
+			return true;
+		}
+		return false;
+	}
+
+	// ThreeOfKind
+	private boolean isThreeOfKind(ArrayList<Card> playerHand) {
+		if ((playerHand.get(0).getRank() == playerHand.get(1).getRank()
+				&& playerHand.get(1).getRank() == playerHand.get(2).getRank())
+				|| (playerHand.get(1).getRank() == playerHand.get(2).getRank()
+						&& playerHand.get(2).getRank() == playerHand.get(3).getRank())
+				|| (playerHand.get(2).getRank() == playerHand.get(3).getRank()
+						&& playerHand.get(3).getRank() == playerHand.get(4).getRank())) {
+
+			highestCard = playerHand.get(2).getValue();
+			return true;
+		}
+		return false;
+	}
+
+	// TwoPair
+	private boolean isTwoPair(ArrayList<Card> playerHand) {
+		int pairCount = 0;
+		int maxPairVal = 0;
+		for (int i = 0; i < playerHand.size() - 1; i++) {
+			if (playerHand.get(i).getValue() == (playerHand.get(i + 1).getValue())) {
+				pairCount++;
+				if (playerHand.get(i).getValue() > maxPairVal) {
+					maxPairVal = playerHand.get(i).getValue();
+				}
+				i++;
+			}
+		}
+		return pairCount == 2;
+	}
+
+	// Pair
+	private boolean isPair(ArrayList<Card> playerHand) {
+		int pairCount = 0;
+		for (int i = 0; i < playerHand.size() - 1; i++) {
+			if (playerHand.get(i).getValue() == (playerHand.get(i + 1).getValue())) {
+				pairCount++;
+				i++;
+				highestCard = playerHand.get(i).getValue();
+			}
+		}
+		return pairCount == 1;
+	}
+
+	public int getHandRank() {
+		if (isRoyalFlush(playerHand)) {
+			return 1;
+		} else if (isStraightFlush(playerHand)) {
+			return 2;
+		} else if (isFourOfKind(playerHand)) {
+			return 3;
+		} else if (isFullHouse(playerHand)) {
+			return 4;
+		} else if (isFlush(playerHand)) {
+			return 5;
+		} else if (isStraight(playerHand)) {
+			return 6;
+		} else if (isThreeOfKind(playerHand)) {
+			return 7;
+		} else if (isTwoPair(playerHand)) {
+			return 8;
+		} else if (isPair(playerHand)) {
+			return 9;
+		}
+		return 10;
+	}
+
+	public ArrayList<Card> getArray() {
+		return playerHand;
+	}
+
 	@Override
 	public int compareTo(PokerHand o) {
 		Collections.sort(o.playerHand);
-		if (this.handRank > o.handRank) {
+		if (this.getHandRank() > o.getHandRank()) {
 			return -1;
-		} else if (this.handRank < o.handRank) {
+		} else if (this.getHandRank() < o.getHandRank()) {
 			return 1;
 		} else {
-
 			if (this.highestCard > o.highestCard) {
 				return 1;
 			} else if (this.highestCard < o.highestCard) {
 				return -1;
 
 			} else {
-				if (this.handRank == 8 && o.handRank == 8 && this.highestCard == o.highestCard) {
-					if (this.highestCard2Pairs > o.highestCard2Pairs) {
-						return 1;
-					} else if (this.highestCard2Pairs < o.highestCard2Pairs) {
+				for (int i = 4; i >= 0; i--) {
+					if (this.playerHand.get(i).getValue() < o.playerHand.get(i).getValue()) {
 						return -1;
-					} else {
-						if (this.kicker2Pairs > o.kicker2Pairs) {
-							return 1;
-						} else if (this.kicker2Pairs < o.kicker2Pairs) {
-							return -1;
-						} else {
-							return 0;
-						}
+					} else if (this.playerHand.get(i).getValue() > o.playerHand.get(i).getValue()) {
+						return 1;
 					}
-
-				} else {
-					for (int i = 4; i >= 0; i--) {
-						if (this.playerHand.get(i) < o.playerHand.get(i)) {
-							return -1;
-						} else if (this.playerHand.get(i) > o.playerHand.get(i)) {
-							return 1;
-						}
-					}
-					return 0;
 				}
+				return 0;
 			}
 		}
 	}
